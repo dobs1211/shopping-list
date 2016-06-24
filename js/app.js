11c1ab448d56ec7c0a904bb9c569b626f594256e
Olga Chavez <olgakatrin827@gmail.com>
2016-06-23 17:50:16 -0700

    final with js

--- js/app.js
+++ js/app.js
@@ -34,15 +34,58 @@ var someGlobalVariable = 0;
 STEP 2
 functions definitions
 ********************************************/
-
-function blahBlah(parameter) {
+/*function to add items in the shopping list*/
+function addItem() {
     //check if the targeting is working
-    alert("I've just activated the blahBlah() function");
+    //alert("I've just activated the addItem() function");
+
+    //get the value of the input box
+    var itemValue = $('#inputBox').val();
+
+    //validate input
+    if (itemValue.length === 0) {
+        alert('You have to add something!!!');
+    }
+
+    //if the input is valid ....
+    else {
+
+        //dynamicaly create one row inside the shopping list
+        var row = $('<li><img class="emptybox" src="images/checkbox-empty.png"><span class="list">' + itemValue + '</span><img class="trash" src="..//shopping-list/images/garbage-white.png"></li>');
+
+        //add each row to the previous ones
+        $('.shopping-list').append(row);
+
+        //empty the input box after submit by resetting the value
+        $('#inputBox').val('');
+    }
 }
 
+/*function to delete all items in the shopping list*/
+function deleteAllItems() {
+    //check if the targeting is working
+    //alert("I've just activated the deleteAllItems() function");
 
-//add item when button pressed
+    //find the UL container (in our case having the class shopping-list) which contains all the LIs and delete all the children inside it
+    $('.shopping-list').empty();
+}
 
+/*function to check items in the shopping list*/
+function checkItem() {
+    //check if the targeting is working
+    //alert("I've just activated the checkItem() function");
+
+    //$(this) means that on WHATEVER you just clicked (the checkbox button), go to the parent of it (in our case the LI above the it) and add / remove the "ticked" class to it
+    $(this).parent().toggleClass('checked');
+}
+
+/*function to delete one item in the shopping list*/
+function deleteAnItem() {
+    //check if the targeting is working
+    //alert("I've just activated the deleteAnItem() function");
+
+    //$(this) means that on WHATEVER you just clicked (the delete one item button), go to the parent of it (in our case the LI above it) and remove the parent and everything inside it
+    $(this).parent().remove();
 }
 
 
@@ -54,11 +97,20 @@ functions usage
 
 $(document).ready(function () {
 
-    /*on click on the "#someHtmlContainer" button activate function called blahBlah with the paramenter 25*/
-    $('#someHtmlContainer').on('click', function () {
-        blahBlah(25);
+    /* on click on the "#addItem" button activate function called addItem() */
+    $('#addItem').on('click', function () {
+        addItem();
+    });
+    /* on click on the ".delete-all" button activate function called deleteAllItems() */
+    $('.delete-all').on('click', function () {
+        deleteAllItems();
     });
 
     //close document ready
 });
-Status API Training Shop Blog About© 2016 GitHub, Inc.Terms Privacy Security Contact Help
+
+/* on click on the ".trash" button activate function called deleteAnItem() */
+$(document).on('click', '.trash', deleteAnItem);
+
+/* on click on the ".emptybox" button activate function called checkItem() */
+$(document).on('click', '.emptybox', checkItem);
